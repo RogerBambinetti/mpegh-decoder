2987a0efe0c8a59f1e4bb068c764385eb111ff94
Roger <rogerbambinetti@hotmail.com>
2025-01-02 23:41:54 -0300

    add callsites

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { execFile } from 'node:child_process';
 import { promisify } from 'node:util';
+import callsites from 'callsites';
 import { InvalidInput, InvalidOutput, PlatformNotSupported } from './error/Errors';
 
 const execFilePromise = promisify(execFile);
@@ -48,7 +49,9 @@ const mpeghDecoder = {
 
             await execFilePromise(path.resolve(__dirname, paths[process.platform]), args);
 
-            return path.resolve(__dirname, IO.output as string);
+            const parentDirname = path.dirname(callsites()[0].getFileName() as string);
+
+            return path.resolve(parentDirname, IO.output as string);
         } catch (error) {
             throw error;
         }
