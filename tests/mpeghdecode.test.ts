871585d73e5090638dc9456b03a7fd4d687a40b5
Roger <rogerbambinetti@hotmail.com>
2025-01-03 23:10:29 -0300

    adding tests

--- tests/mpeghdecode.test.ts
+++ tests/mpeghdecode.test.ts
@@ -1,18 +1,44 @@
-import mpeghdecoder from "../src";
+import { decode, bulkDecode } from "../src";
 import fs from 'fs';
 import path from 'path';
 
-describe("mpegh-decoder tests", async () => {
-    test("should bulk decode multiple files", async () => {
-        const files: string[] = fs.readdirSync('./files');
+describe("mpegh-decoder tests", () => {
 
-        const test = files.map(file => ({
-            input: `./files/${file}`,
-            output: `./output/${file.replace(path.extname(file), '.wav')}`
-        }));
+    const inputFilePath = './tests/sample.m4a';
+    const outputPath = './tests/output';
 
-        await mpeghdecoder.bulkDecode(test);
-
-        console.log('Terminou');
+    beforeAll(() => {
+        // Create output dir
+        fs.mkdirSync(outputPath);
     });
+
+    afterEach(() => {
+        // Remove output files generated from tests
+        fs.rmSync(outputPath, { recursive: true });
+
+        // Recreate output dir
+        fs.mkdirSync(outputPath);
+    });
+
+    afterAll(() => {
+        // Remove output files generated from tests
+        fs.rmSync(outputPath, { recursive: true });
+    });
+
+    test("should decode file with specified output path", async () => {
+        const { outputFilePath } = await decode({ input: inputFilePath, output: path.resolve(outputPath, 'output.wav') });
+
+        expect(fs.existsSync(outputFilePath)).toBe(true);
+    });
+
+    test("should decode file without specified output path", async () => {
+        const { outputFilePath } = await decode({ input: inputFilePath });
+
+        expect(fs.existsSync(outputFilePath)).toBe(true);
+    });
+
+    test("should not decode file without specified input path", async () => {
+
+    });
+
 });
