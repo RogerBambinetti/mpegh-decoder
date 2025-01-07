e01818b2ee65ecb91d83d3988a8122a3eb2ab6fc
Roger <rogerbambinetti@hotmail.com>
2025-01-07 18:59:14 -0300

    adding mp4 sample for testing

--- tests/mpeghdecode.test.ts
+++ tests/mpeghdecode.test.ts
@@ -6,16 +6,25 @@ describe("mpegh-decoder tests", () => {
 
     const testTimeout = 1000 * 60;
 
-    const inputFilePath = './tests/sample.m4a';
+    const inputFilePath = './tests/sample.mp4';
+    const outputs: string[] = [];
+
+    afterAll(() => {
+        outputs.forEach(outputFilePath => {
+            fs.unlinkSync(outputFilePath);
+        });
+    });
 
     test("should decode file with specified output path", async () => {
         const { outputFilePath } = await decode({ input: path.resolve(inputFilePath), output: path.resolve('./tests/output.wav') });
+        outputs.push(outputFilePath);
 
         expect(fs.existsSync(outputFilePath)).toBe(true);
     }, testTimeout);
 
     test("should decode file without specified output path", async () => {
         const { outputFilePath } = await decode({ input: path.resolve(inputFilePath) });
+        outputs.push(outputFilePath);
 
         expect(fs.existsSync(outputFilePath)).toBe(true);
     }, testTimeout);
